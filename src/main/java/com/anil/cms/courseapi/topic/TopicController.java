19179644d784e636b9fa419942c11a8ab8817b5c
poudyalanil <anilrunjakote@gmail.com>
2021-06-04 19:18:25 +0545

    Post request

--- src/main/java/com/anil/cms/courseapi/topic/TopicController.java
+++ src/main/java/com/anil/cms/courseapi/topic/TopicController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,6 +31,11 @@ public class TopicController {
     @RequestMapping("/topics/{id}")
     public Topic getTopicic(@PathVariable String id) {
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value="/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
     }
     
 }
--- src/main/java/com/anil/cms/courseapi/topic/TopicService.java
+++ src/main/java/com/anil/cms/courseapi/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.anil.cms.courseapi.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-    List<Topic> topics = Arrays.asList(
+    List<Topic> topics = new ArrayList<>(Arrays.asList(
               new Topic("big-data","Big Data","Study of big data technolgis and underlying techniques"), 
               new Topic("ai","Artificial Intelligence","Study of machine learning and artificial intelligence"), 
               new Topic("database-studies","Database Studies","Study of database magement systems"), 
               new Topic("comp-mathematics","Computational Mathematics","Study of big mathematics used during computations")
-    );
+    ));
         
     public List<Topic> getAllTopics() {
         return topics;
@@ -21,5 +22,9 @@ public class TopicService {
 
     public Topic getTopic(String id) {
        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
