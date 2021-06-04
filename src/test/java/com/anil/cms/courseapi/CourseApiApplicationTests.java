472f2b7f7770aa125138ef993e45fafee8392069
poudyalanil <anilrunjakote@gmail.com>
2021-06-04 07:45:34 +0545

    Initial setup

---
+++ src/test/java/com/anil/cms/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anil.cms.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
