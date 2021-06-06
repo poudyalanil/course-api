659cff4dd97812fa8bed7716ea9bd9450688d1c9
poudyalanil <anilrunjakote@gmail.com>
2021-06-06 10:44:27 +0545

    Put req

--- src/main/java/com/anil/cms/courseapi/topic/TopicController.java
+++ src/main/java/com/anil/cms/courseapi/topic/TopicController.java
@@ -34,8 +34,12 @@ public class TopicController {
     }
 
     @RequestMapping(method = RequestMethod.POST,value="/topics")
-    public void addTopic(@RequestBody Topic topic){
+    public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
     }
     
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopics(id, topic);
+    }
 }
